979b84ee415173810c43ae140c6d04759a85b23e
wao218 <wesleyosborne1@gmail.com>
2023-11-01 15:50:54 -0700

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by wesley osborne on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
