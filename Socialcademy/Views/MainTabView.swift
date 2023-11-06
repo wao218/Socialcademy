cf9ebd20a0ed5943f0eec53702b80cdd9e38f8cd
wao218 <wesleyosborne1@gmail.com>
2023-11-05 19:09:26 -0800

    Add MainTabView to display both Favorites and All Posts

---
+++ Socialcademy/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  Socialcademy
+//
+//  Created by wesley osborne on 11/5/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
